12bbb5380e4da0f353a32f94aaaeb82345131745
Pawanks7861 <happyk682@gmail.com>
2024-11-14 23:31:22 +0530

    changes

--- modules/purchase/models/Purchase_model.php
+++ modules/purchase/models/Purchase_model.php
@@ -4545,8 +4545,6 @@ class Purchase_model extends App_Model
 
         $pur_order = $this->get_pur_order($pur_order_id);
         $pur_order_detail = $this->get_pur_order_detail($pur_order_id);
-        $estimate_details = $this->get_pur_estimate_detail($pur_order_id);
-        $tax_data = $this->get_html_tax_pur_invoice($pur_order_id);
         $company_name = get_option('invoice_company_name');
         $address = get_option('invoice_company_address');
         $day = date('d', strtotime($pur_order->order_date));
@@ -4669,19 +4667,19 @@ class Purchase_model extends App_Model
       </table><br><br>';
 
         $html .= '<table class="table text-right"><tbody>';
-        // if ($pur_order->discount_total > 0) {
+        if ($pur_order->discount_total > 0) {
             $html .= '<tr id="subtotal">
                     <td width="33%"></td>
                      <td>' . _l('subtotal') . ' </td>
                      <td class="subtotal">
-                        ' . app_format_money($estimate_details['subtotal'], '') . '
+                        ' . app_format_money($t_mn, '') . '
                      </td>
                   </tr>
                   <tr id="subtotal">
                   <td width="33%"></td>
                      <td>' . _l('discount(%)') . '(%)' . '</td>
                      <td class="subtotal">
-                        ' . pur_html_entity_decode($tax_data['preview_html']) . ' %' . '
+                        ' . app_format_money($pur_order->discount_percent, '') . ' %' . '
                      </td>
                   </tr>
                   <tr id="subtotal">
@@ -4691,12 +4689,12 @@ class Purchase_model extends App_Model
                         ' . app_format_money($pur_order->discount_total, '') . '
                      </td>
                   </tr>';
-        // }
+        }
         $html .= '<tr id="subtotal">
                  <td width="33%"></td>
                  <td><strong>' . _l('total') . '</strong></td>
                  <td class="subtotal">
-                    ' .'₹ '. app_format_money($estimate_details['total'], '') . '
+                    ' .'₹ '. app_format_money($pur_order->total, '') . '
                  </td>
               </tr>';
 
