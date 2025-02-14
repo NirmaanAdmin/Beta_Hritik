3fcae34039b05172ff00e68f1034ba0b98dc3c7c
Pawan Kumar <happyk682@gmail.com>
2025-02-14 13:12:11 +0530

    changes

--- modules/purchase/assets/js/manage_invoices.js
+++ modules/purchase/assets/js/manage_invoices.js
@@ -15,7 +15,7 @@ var expenseDropzone;
     "billing_status": "[name='billing_status']",
   };
 
-  initDataTable(table_invoice, admin_url + 'purchase/table_pur_invoices', [], [], Params, [5, 'desc']);
+  initDataTable(table_invoice, admin_url + 'purchase/table_pur_invoices', [], [], Params, [6, 'desc']);
   $.each(Params, function (i, obj) {
     $('select' + obj).on('change', function () {
       table_invoice.DataTable().ajax.reload()
