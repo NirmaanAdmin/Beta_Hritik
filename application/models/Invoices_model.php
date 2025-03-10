47c1dcdc3fd8c10b8c57c7655178bada3a995c55
Jakirhuseain <jakirhuseain530@gmail.com>
2025-03-10 10:57:40 +0530

    Change

--- application/models/Invoices_model.php
+++ application/models/Invoices_model.php
@@ -2327,7 +2327,7 @@ class Invoices_model extends App_Model
                         if (!is_numeric($item['qty'])) {
                             $item['qty'] = 1;
                         }
-                        $amount = $item['rate'] * $item['qty'];
+                        $amount = ($item['rate'] * $item['qty']) + $item['tax'];
                         $total_tax = $item['tax'];
                         $annexure = $item['annexure'];
                         $itemid = $item['id'];
