a8aef3fe354c4d5c30124c17ba5024284ec2f4f2
Pawanks7861 <happyk682@gmail.com>
2025-03-24 17:41:29 +0530

    adding tinymce editor to meeting agenda from and table by  default

--- modules/meeting_management/views/agenda_form.php
+++ modules/meeting_management/views/agenda_form.php
@@ -5,92 +5,123 @@
 <div id="wrapper">
    <div class="content">
       <div class="row">
-         <div class="col-md-5 left-column">
+         <div class="col-md-12 left-column">
             <div class="panel_s">
                <div class="panel-body">
-               <?php echo form_open(admin_url('meeting_management/agendaController/create'), array('id' => 'agenda-submit-form')); ?>
+                  <?php echo form_open(admin_url('meeting_management/agendaController/create'), array('id' => 'agenda-submit-form')); ?>
 
-               <!-- Client Dropdown -->
-               <div class="form-group">
-                  <label for="client_id"><?php echo _l('select_client'); ?></label>
-                  <select id="client_id" name="client_id" class="form-control" required>
-                     <option value=""><?php echo _l('select_client'); ?></option>
-                     <?php foreach ($clients as $client) : ?>
-                        <option value="<?php echo $client['userid']; ?>">
-                           <?php echo $client['company']; ?>
-                        </option>
-                     <?php endforeach; ?>
-                  </select>
+                  <!-- Client Dropdown -->
+                  <div class="form-group">
+                     <label for="client_id"><?php echo _l('select_client'); ?></label>
+                     <select id="client_id" name="client_id" class="form-control" required>
+                        <option value=""><?php echo _l('select_client'); ?></option>
+                        <?php foreach ($clients as $client) : ?>
+                           <option value="<?php echo $client['userid']; ?>">
+                              <?php echo $client['company']; ?>
+                           </option>
+                        <?php endforeach; ?>
+                     </select>
+                  </div>
+
+                  <!-- Project Dropdown (Initially empty, populated via Ajax) -->
+                  <div class="form-group">
+                     <label for="project_id"><?php echo _l('select_project'); ?></label>
+                     <select id="project_id" name="project_id" class="form-control" required>
+                        <option value=""><?php echo _l('select_project'); ?></option>
+                     </select>
+                  </div>
+
+                  <!-- Meeting Title -->
+                  <div class="form-group">
+                     <label for="meeting_title"><?php echo _l('meeting_title'); ?></label>
+                     <input type="text" id="meeting_title" name="meeting_title" class="form-control" required>
+                  </div>
+
+                  <!-- Meeting Date -->
+                  <div class="form-group">
+                     <label for="meeting_date"><?php echo _l('meeting_date'); ?></label>
+                     <input type="datetime-local" id="meeting_date" name="meeting_date" class="form-control" required>
+                  </div>
+
+                  <!-- Agenda -->
+                  <div class="form-group">
+                     <label for="agenda"><?php echo _l('meeting_agenda'); ?></label>
+                     <!-- <textarea id="agenda" name="agenda" class="form-control" required></textarea>  -->
+                     <?php
+                     $deafult_val = '
+<table border="1" cellspacing="0" cellpadding="5" style="width:100%; border-collapse: collapse;">
+    <thead>
+        <tr>
+            <th>Sr. No.</th>
+            <th>Area</th>
+            <th>Description</th>
+            <th>Decision</th>
+            <th>Action</th>
+            <th>Action By</th>
+            <th>Target Date</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table><br>';
+                     ?>
+
+                     <?php echo render_textarea('agenda', '', $deafult_val, array(), array(), 'mtop15', 'tinymce'); ?>
+
+                  </div>
+
+                  <!-- Submit Button -->
+                  <div class="btn-bottom-toolbar text-right">
+                     <button type="submit" class="btn btn-info"><?php echo _l('submit'); ?></button>
+                  </div>
+
+                  <?php echo form_close(); ?>
                </div>
-
-               <!-- Project Dropdown (Initially empty, populated via Ajax) -->
-               <div class="form-group">
-                  <label for="project_id"><?php echo _l('select_project'); ?></label>
-                  <select id="project_id" name="project_id" class="form-control" required>
-                     <option value=""><?php echo _l('select_project'); ?></option>
-                  </select>
-               </div>
-
-               <!-- Meeting Title -->
-               <div class="form-group">
-                  <label for="meeting_title"><?php echo _l('meeting_title'); ?></label>
-                  <input type="text" id="meeting_title" name="meeting_title" class="form-control" required>
-               </div>
-
-               <!-- Meeting Date -->
-               <div class="form-group">
-                  <label for="meeting_date"><?php echo _l('meeting_date'); ?></label>
-                  <input type="datetime-local" id="meeting_date" name="meeting_date" class="form-control" required>
-               </div>
-
-               <!-- Agenda -->
-               <div class="form-group">
-                  <label for="agenda"><?php echo _l('meeting_agenda'); ?></label>
-                  <textarea id="agenda" name="agenda" class="form-control" required></textarea>
-               </div>
-
-               <!-- Submit Button -->
-               <div class="btn-bottom-toolbar text-right">
-                  <button type="submit" class="btn btn-info"><?php echo _l('submit'); ?></button>
-               </div>
-
-               <?php echo form_close(); ?>
             </div>
          </div>
       </div>
    </div>
-</div>
 
-<?php init_tail(); ?>
+   <?php init_tail(); ?>
 
-<!-- jQuery to handle client selection and load projects -->
-<script>
-$(document).ready(function() {
-    $('#client_id').on('change', function() {
-        var client_id = $(this).val();
-        if(client_id) {
-            $.ajax({
-                url: '<?php echo admin_url("meeting_management/agendaController/get_projects_by_client/"); ?>' + client_id,
-                type: 'GET',
-                dataType: 'json',
-                success: function(data) {
-                    $('#project_id').empty();
-                    $('#project_id').append('<option value=""><?php echo _l("select_project"); ?></option>');
-                    $.each(data, function(key, value) {
-                        $('#project_id').append('<option value="'+ value.id +'">'+ value.name +'</option>');
-                    });
-                },
-                error: function() {
-                    alert('Error retrieving projects');
-                }
-            });
-        } else {
-            $('#project_id').empty();
-            $('#project_id').append('<option value=""><?php echo _l("select_project"); ?></option>');
-        }
-    });
-});
-</script>
+   <!-- jQuery to handle client selection and load projects -->
+   <script>
+      $(document).ready(function() {
+         $('#client_id').on('change', function() {
+            var client_id = $(this).val();
+            if (client_id) {
+               $.ajax({
+                  url: '<?php echo admin_url("meeting_management/agendaController/get_projects_by_client/"); ?>' + client_id,
+                  type: 'GET',
+                  dataType: 'json',
+                  success: function(data) {
+                     $('#project_id').empty();
+                     $('#project_id').append('<option value=""><?php echo _l("select_project"); ?></option>');
+                     $.each(data, function(key, value) {
+                        $('#project_id').append('<option value="' + value.id + '">' + value.name + '</option>');
+                     });
+                  },
+                  error: function() {
+                     alert('Error retrieving projects');
+                  }
+               });
+            } else {
+               $('#project_id').empty();
+               $('#project_id').append('<option value=""><?php echo _l("select_project"); ?></option>');
+            }
+         });
+      });
+   </script>
 
-</body>
-</html>
+   </body>
+
+   </html>
