f5eb58c502382936a5a244c1c5718cc10aadc76a
Pawanks7861 <happyk682@gmail.com>
2025-04-01 18:22:21 +0530

    changes

--- modules/purchase/views/manage_goods_receipt/table_manage_goods_receipt.php
+++ modules/purchase/views/manage_goods_receipt/table_manage_goods_receipt.php
@@ -40,11 +40,11 @@ if (isset($kind)) {
 }
 
 if (isset($delivery)) {
-    if($delivery == "undelivered"){
+    if ($delivery == "undelivered") {
         $where[] = 'AND delivery_status = "0"';
-    } else if($delivery == "partially_delivered"){
+    } else if ($delivery == "partially_delivered") {
         $where[] = 'AND delivery_status = "1"';
-    } else if($delivery == "completely_delivered"){
+    } else if ($delivery == "completely_delivered") {
         $where[] = 'AND delivery_status = "2"';
     } else {
         $where[] = 'AND delivery_status = "0"';
@@ -52,7 +52,7 @@ if (isset($delivery)) {
 }
 
 $result = data_tables_purchase_tracker_init($aColumns, $join, $where, [
-   'type',
+    'type',
 ]);
 
 $output  = $result['output'];
@@ -64,17 +64,14 @@ foreach ($rResult as $aRow) {
         $_data = $aRow[$aColumns[$i]];
         if ($aColumns[$i] == 'supplier_name') {
             $_data = wh_get_vendor_company_name($aRow['supplier_name']);
-        }
-         
-        elseif ($aColumns[$i] == 'date_add') {
+        } elseif ($aColumns[$i] == 'date_add') {
             $_data = date('d M, Y', strtotime($aRow['date_add']));
-        } elseif($aColumns[$i] == 'goods_receipt_code') {
+        } elseif ($aColumns[$i] == 'goods_receipt_code') {
             $name = '';
-            if(!empty($aRow['goods_receipt_code'])) {
+            if (!empty($aRow['goods_receipt_code'])) {
                 $name .= '<a href="' . admin_url('purchase/view_purchase/' . $aRow['id']) . '" 
-                onclick="init_goods_receipt(' . $aRow['id'] . '); small_table_full_view(); return false;">' . 
-                $aRow['goods_receipt_code'] . '</a>';
-     
+                onclick="init_goods_receipt(' . $aRow['id'] . '); small_table_full_view(); return false;">' .
+                    $aRow['goods_receipt_code'] . '</a>';
             } else {
                 $name .= '<a href="' . admin_url('purchase/view_po_tracker/' . $aRow['id']) . '" onclick="init_po_tracker(' . $aRow['id'] . '); small_table_full_view(); return false;">' . _l('Update') . '</a>';
             }
@@ -91,20 +88,25 @@ foreach ($rResult as $aRow) {
                 }
             }
             $_data = $name;
-        }elseif ($aColumns[$i] == 'buyer_id') {
-            $get_production_status = get_production_status($aRow['id']);
-            $_data = $get_production_status;
-        }  elseif ($aColumns[$i] == 'delivery_status') {
+        } elseif ($aColumns[$i] == 'buyer_id') {
+            if ($aRow['type'] != 2) {
+
+                $get_production_status = get_production_status($aRow['id']);
+                $_data = $get_production_status;
+            }else{
+                $_data = '<span class="inline-block label label-danger">Not Strarted</span>';
+            }
+        } elseif ($aColumns[$i] == 'delivery_status') {
             $delivery_status = '';
-            if($aRow['delivery_status'] == 0) {
-                $delivery_status = '<span class="inline-block label label-danger" task-status-table="undelivered">'._l('undelivered');
-            } else if($aRow['delivery_status'] == 1) {
-                $delivery_status = '<span class="inline-block label label-warning" task-status-table="partially_delivered">'._l('partially_delivered');
+            if ($aRow['delivery_status'] == 0) {
+                $delivery_status = '<span class="inline-block label label-danger" task-status-table="undelivered">' . _l('undelivered');
+            } else if ($aRow['delivery_status'] == 1) {
+                $delivery_status = '<span class="inline-block label label-warning" task-status-table="partially_delivered">' . _l('partially_delivered');
             } else {
-                $delivery_status = '<span class="inline-block label label-success" task-status-table="completely_delivered">'._l('completely_delivered');
+                $delivery_status = '<span class="inline-block label label-success" task-status-table="completely_delivered">' . _l('completely_delivered');
             }
             $_data = $delivery_status;
-        } 
+        }
 
         $row[] = $_data;
     }
